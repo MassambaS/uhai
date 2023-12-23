97bb8fc8283044849c793de7a60be4ec0e74479f
massambas <massamba.sall@student.unamur.be>
2023-12-23 20:44:07 +0100

    change db

--- shmis_api/settings.py
+++ shmis_api/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'shmis_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'uhaidb',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
